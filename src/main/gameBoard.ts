bdb0bc51ebde0d3d1b876405519e30ae863a8c58
Mustafa Hussein <mustafa.hussein@cazoo.co.uk>
2021-12-09 09:48:59 +0000

    mob next [ci-skip] [ci skip] [skip ci]

--- src/main/gameBoard.ts
+++ src/main/gameBoard.ts
@@ -1,47 +1,75 @@
-import {GamePiece, GridBoard, Location} from "./types";
+import { GamePiece, GridBoard, Location } from "./types";
 
 export class GameBoard {
-    private grid: GridBoard = {
-        [Location.TOP_LEFT]: GamePiece.UNSET,
-        [Location.TOP_MIDDLE]: GamePiece.UNSET,
-        [Location.TOP_RIGHT]: GamePiece.UNSET,
-        [Location.CENTRE_LEFT]: GamePiece.UNSET,
-        [Location.CENTRE_MIDDLE]: GamePiece.UNSET,
-        [Location.CENTRE_RIGHT]: GamePiece.UNSET,
-        [Location.BOTTOM_LEFT]: GamePiece.UNSET,
-        [Location.BOTTOM_MIDDLE]: GamePiece.UNSET,
-        [Location.BOTTOM_RIGHT]: GamePiece.UNSET
-    }
+  private grid: GridBoard = {
+    [Location.TOP_LEFT]: GamePiece.UNSET,
+    [Location.TOP_MIDDLE]: GamePiece.UNSET,
+    [Location.TOP_RIGHT]: GamePiece.UNSET,
+    [Location.CENTRE_LEFT]: GamePiece.UNSET,
+    [Location.CENTRE_MIDDLE]: GamePiece.UNSET,
+    [Location.CENTRE_RIGHT]: GamePiece.UNSET,
+    [Location.BOTTOM_LEFT]: GamePiece.UNSET,
+    [Location.BOTTOM_MIDDLE]: GamePiece.UNSET,
+    [Location.BOTTOM_RIGHT]: GamePiece.UNSET,
+  };
 
-    public setLocation(location: Location, piece: GamePiece) {
-        this.grid[location] = piece;
-    }
+  public setLocation(location: Location, piece: GamePiece) {
+    this.grid[location] = piece;
+  }
 
-    public isLocationFree(location: Location): boolean {
-        return this.grid[location] === GamePiece.UNSET
-    }
+  public isLocationFree(location: Location): boolean {
+    return this.grid[location] === GamePiece.UNSET;
+  }
 
-    private checkForLeftColumnXWin(): boolean {
-        return this.grid[Location.TOP_LEFT] === GamePiece.X &&
-          this.grid[Location.CENTRE_LEFT] === GamePiece.X &&
-          this.grid[Location.BOTTOM_LEFT] === GamePiece.X
-    }
+  private checkForLeftColumnXWin(): boolean {
+    return (
+      this.grid[Location.TOP_LEFT] === GamePiece.X &&
+      this.grid[Location.CENTRE_LEFT] === GamePiece.X &&
+      this.grid[Location.BOTTOM_LEFT] === GamePiece.X
+    );
+  }
 
-    private checkForMiddleColumnXWin(): boolean {
-        return this.grid[Location.TOP_MIDDLE] === GamePiece.X &&
-          this.grid[Location.CENTRE_MIDDLE] === GamePiece.X &&
-          this.grid[Location.BOTTOM_MIDDLE] === GamePiece.X
-    }
+  private checkForMiddleColumnXWin(): boolean {
+    return (
+      this.grid[Location.TOP_MIDDLE] === GamePiece.X &&
+      this.grid[Location.CENTRE_MIDDLE] === GamePiece.X &&
+      this.grid[Location.BOTTOM_MIDDLE] === GamePiece.X
+    );
+  }
 
-    public checkForXWin(): boolean {
-        return this.checkForMiddleColumnXWin() || this.checkForLeftColumnXWin()
-    }
+  private checkForRightColumnXWin(): boolean {
+    return (
+      this.grid[Location.TOP_RIGHT] === GamePiece.X &&
+      this.grid[Location.CENTRE_RIGHT] === GamePiece.X &&
+      this.grid[Location.BOTTOM_RIGHT] === GamePiece.X
+    );
+  }
 
-    public toArray(): string[][] {
-        return [
-            [this.grid[Location.TOP_LEFT], this.grid[Location.TOP_MIDDLE], this.grid[Location.TOP_RIGHT]],
-            [this.grid[Location.CENTRE_LEFT], this.grid[Location.CENTRE_MIDDLE], this.grid[Location.CENTRE_RIGHT]],
-            [this.grid[Location.BOTTOM_LEFT], this.grid[Location.BOTTOM_MIDDLE], this.grid[Location.BOTTOM_RIGHT]],
-        ]
-    }
+  public checkForXWin(): boolean {
+    return (
+      this.checkForMiddleColumnXWin() ||
+      this.checkForLeftColumnXWin() ||
+      this.checkForRightColumnXWin()
+    );
+  }
+
+  public toArray(): string[][] {
+    return [
+      [
+        this.grid[Location.TOP_LEFT],
+        this.grid[Location.TOP_MIDDLE],
+        this.grid[Location.TOP_RIGHT],
+      ],
+      [
+        this.grid[Location.CENTRE_LEFT],
+        this.grid[Location.CENTRE_MIDDLE],
+        this.grid[Location.CENTRE_RIGHT],
+      ],
+      [
+        this.grid[Location.BOTTOM_LEFT],
+        this.grid[Location.BOTTOM_MIDDLE],
+        this.grid[Location.BOTTOM_RIGHT],
+      ],
+    ];
+  }
 }
